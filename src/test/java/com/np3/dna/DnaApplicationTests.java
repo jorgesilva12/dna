e3babe75f9078491c82f05ce02ea349f63979d4e
jorge silva <jorgelafaiete.silva@gmail.com>
2023-08-12 14:22:11 +0100

    Initial commit

---
+++ src/test/java/com/np3/dna/DnaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.np3.dna;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DnaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
