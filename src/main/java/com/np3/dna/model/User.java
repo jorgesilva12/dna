fbf120a35d25d39fb06231612e709a791de01fcc
jorge silva <jorgelafaiete.silva@gmail.com>
2023-08-19 14:41:57 +0100

    User table

--- src/main/java/com/np3/dna/model/User.java
+++ src/main/java/com/np3/dna/model/User.java
@@ -1,16 +1,31 @@
 package com.np3.dna.model;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
-
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "np3_user")
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private UUID id;
     private String name;
     private String username;
     private String password;
     private String email;
     private Long number;
+    @ManyToMany
+    @JoinTable(name = "np3_user_role",
+        joinColumns = @JoinColumn(name = "user_id"),
+        inverseJoinColumns = @JoinColumn(name = "role_id"))
     private List<Role> roles = new ArrayList<>();
 
 }
